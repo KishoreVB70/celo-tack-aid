ae362df804968297bf7866aae76f99e9ef2801db
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-09-12 21:28:51 +0400

    improvements

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,24 +1,19 @@
-import React from 'react';
-
-    
-
+import React from "react";
 
 export const NavigationBar = (props) => {
-  return <div>
-
-
-
-    
-    <nav class="navbar">
-        <div class="container-fluid">
-        <span class="navbar-brand m-0 fw-bold text-white"
-            ><h1>Tack Aid</h1></span
-          >
-          <span class="nav-link border rounded-pill bg-light">
-            <span id='balance'>{props.cUSDBalance}</span>
-            cUSD
-          </span>
-        </div>
-      </nav>
-  </div>;
+	return (
+		<div>
+			<nav className="navbar">
+				<div className="container-fluid">
+					<span className="navbar-brand m-0 fw-bold text-white">
+						<h1>Tack Aid</h1>
+					</span>
+					<span className="nav-link border rounded-pill bg-light">
+						<span id="balance">{props.cUSDBalance}</span>
+						cUSD
+					</span>
+				</div>
+			</nav>
+		</div>
+	);
 };
